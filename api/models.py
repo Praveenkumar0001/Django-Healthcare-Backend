f7c08763c572d0ce16ba48084bb43d19f0f30285
Praveenkumar0001 <129545201+Praveenkumar0001@users.noreply.github.com>
2026-02-17 21:24:57 +0530

    Update models and test patient creation successfully

--- api/models.py
+++ api/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import EmailValidator, RegexValidator
-
-
 class Patient(models.Model):
     """
     Patient model to store patient information.
